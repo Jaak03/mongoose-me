a4d662b55d8e4c8ac52149faebd55fcba8f2c63e
jaak <jacques@mohara.co>
2020-09-16 20:24:37 +0200

    [ADDED] It is working now, added some samples there for you to look at in the index file.

--- index.js
+++ index.js
@@ -16,9 +16,63 @@ let mongoClient;
   mongoClient = await getClient(mongoClient);
   const res = (await dbAction(
     'Tag',
+
+    // BASIC FIND
     {
-      'find': [{}],
+      'find': [{
+        name: null
+      }],
     },
+
+    // MORE COMPLEX STATEMENTS
+    // {
+    //   'findOneAndUpdate': [
+    //     { name: null },
+    //     {
+    //       $set: {
+    //         name: 'no-name-tag',
+    //       }
+    //     },
+    //     {
+    //       many: true,
+    //     }
+    //   ]
+    // },
+
+    // {
+    //   'updateMany': [
+    //     { name: null },
+    //     {
+    //       $set: {
+    //         name: 'no-name-tag',
+    //       }
+    //     },
+    //     {
+    //       runValidators: true,
+    //     },
+    //   ]
+    // },
+
+    // AND AGGREGATION, CHECK OUT THE DOUBLE []
+    // {
+    //   'aggregate': [[
+    //     {
+    //       $match: {},
+    //     },
+    //     {
+    //       $group: {
+    //         _id: '$name',
+    //       },
+    //     },
+    //     {
+    //       $project: {
+    //         _id: 0,
+    //         name: '$_id',
+    //       },
+    //     },
+    //   ]],
+    // },
+
     mongoClient,
   )).flat();
 
